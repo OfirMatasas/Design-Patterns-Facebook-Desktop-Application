fed563def064f9a8ec0beba191b69d7797b49b5b
OfirMatasas <ofir.matasas@cyberark.com>
2022-08-06 12:09:11 +0300

    Add profile information

--- 02_SolutionStarter.V8.0.2.2/FacebookWinFormsApp/Forms/FormProfile.Designer.cs
+++ 02_SolutionStarter.V8.0.2.2/FacebookWinFormsApp/Forms/FormProfile.Designer.cs
@@ -31,6 +31,12 @@
             this.pictureBoxProfilePicture = new System.Windows.Forms.PictureBox();
             this.pictureBoxCoverPicture = new System.Windows.Forms.PictureBox();
             this.labelProfileName = new System.Windows.Forms.Label();
+            this.labelBirthday = new System.Windows.Forms.Label();
+            this.labelLocation = new System.Windows.Forms.Label();
+            this.labelFriendsCount = new System.Windows.Forms.Label();
+            this.labelHometown = new System.Windows.Forms.Label();
+            this.labelGender = new System.Windows.Forms.Label();
+            this.labelWallPosts = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfilePicture)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCoverPicture)).BeginInit();
             this.SuspendLayout();
@@ -39,10 +45,10 @@
             // 
             this.pictureBoxProfilePicture.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBoxProfilePicture.Location = new System.Drawing.Point(481, 176);
+            this.pictureBoxProfilePicture.Location = new System.Drawing.Point(476, 146);
             this.pictureBoxProfilePicture.Name = "pictureBoxProfilePicture";
-            this.pictureBoxProfilePicture.Size = new System.Drawing.Size(320, 302);
-            this.pictureBoxProfilePicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxProfilePicture.Size = new System.Drawing.Size(320, 352);
+            this.pictureBoxProfilePicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxProfilePicture.TabIndex = 0;
             this.pictureBoxProfilePicture.TabStop = false;
             // 
@@ -50,9 +56,10 @@
             // 
             this.pictureBoxCoverPicture.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBoxCoverPicture.Location = new System.Drawing.Point(180, 53);
+            this.pictureBoxCoverPicture.Location = new System.Drawing.Point(66, 23);
             this.pictureBoxCoverPicture.Name = "pictureBoxCoverPicture";
-            this.pictureBoxCoverPicture.Size = new System.Drawing.Size(936, 258);
+            this.pictureBoxCoverPicture.Size = new System.Drawing.Size(1152, 356);
+            this.pictureBoxCoverPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxCoverPicture.TabIndex = 1;
             this.pictureBoxCoverPicture.TabStop = false;
             // 
@@ -62,18 +69,102 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelProfileName.AutoSize = true;
             this.labelProfileName.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelProfileName.Location = new System.Drawing.Point(617, 638);
+            this.labelProfileName.Location = new System.Drawing.Point(439, 536);
             this.labelProfileName.Name = "labelProfileName";
             this.labelProfileName.Size = new System.Drawing.Size(401, 59);
             this.labelProfileName.TabIndex = 2;
             this.labelProfileName.Text = "Firstname Lastname";
             this.labelProfileName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // labelBirthday
+            // 
+            this.labelBirthday.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelBirthday.AutoSize = true;
+            this.labelBirthday.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBirthday.Location = new System.Drawing.Point(187, 667);
+            this.labelBirthday.Name = "labelBirthday";
+            this.labelBirthday.Size = new System.Drawing.Size(203, 59);
+            this.labelBirthday.TabIndex = 3;
+            this.labelBirthday.Text = "Birthday: ";
+            this.labelBirthday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelLocation
+            // 
+            this.labelLocation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelLocation.AutoSize = true;
+            this.labelLocation.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLocation.Location = new System.Drawing.Point(755, 741);
+            this.labelLocation.Name = "labelLocation";
+            this.labelLocation.Size = new System.Drawing.Size(207, 59);
+            this.labelLocation.TabIndex = 4;
+            this.labelLocation.Text = "Location: ";
+            this.labelLocation.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelFriendsCount
+            // 
+            this.labelFriendsCount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelFriendsCount.AutoSize = true;
+            this.labelFriendsCount.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFriendsCount.Location = new System.Drawing.Point(187, 815);
+            this.labelFriendsCount.Name = "labelFriendsCount";
+            this.labelFriendsCount.Size = new System.Drawing.Size(181, 59);
+            this.labelFriendsCount.TabIndex = 5;
+            this.labelFriendsCount.Text = "Friends: ";
+            this.labelFriendsCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelHometown
+            // 
+            this.labelHometown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelHometown.AutoSize = true;
+            this.labelHometown.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHometown.Location = new System.Drawing.Point(755, 667);
+            this.labelHometown.Name = "labelHometown";
+            this.labelHometown.Size = new System.Drawing.Size(256, 59);
+            this.labelHometown.TabIndex = 6;
+            this.labelHometown.Text = "Hometown: ";
+            this.labelHometown.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelGender
+            // 
+            this.labelGender.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelGender.AutoSize = true;
+            this.labelGender.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGender.Location = new System.Drawing.Point(187, 741);
+            this.labelGender.Name = "labelGender";
+            this.labelGender.Size = new System.Drawing.Size(184, 59);
+            this.labelGender.TabIndex = 7;
+            this.labelGender.Text = "Gender: ";
+            this.labelGender.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelWallPosts
+            // 
+            this.labelWallPosts.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelWallPosts.AutoSize = true;
+            this.labelWallPosts.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWallPosts.Location = new System.Drawing.Point(755, 815);
+            this.labelWallPosts.Name = "labelWallPosts";
+            this.labelWallPosts.Size = new System.Drawing.Size(236, 59);
+            this.labelWallPosts.TabIndex = 8;
+            this.labelWallPosts.Text = "Wall Posts: ";
+            this.labelWallPosts.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // FormProfile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1274, 1029);
+            this.Controls.Add(this.labelWallPosts);
+            this.Controls.Add(this.labelGender);
+            this.Controls.Add(this.labelHometown);
+            this.Controls.Add(this.labelFriendsCount);
+            this.Controls.Add(this.labelLocation);
+            this.Controls.Add(this.labelBirthday);
             this.Controls.Add(this.labelProfileName);
             this.Controls.Add(this.pictureBoxProfilePicture);
             this.Controls.Add(this.pictureBoxCoverPicture);
@@ -91,5 +182,11 @@
         private System.Windows.Forms.PictureBox pictureBoxProfilePicture;
         private System.Windows.Forms.PictureBox pictureBoxCoverPicture;
         private System.Windows.Forms.Label labelProfileName;
+        private System.Windows.Forms.Label labelBirthday;
+        private System.Windows.Forms.Label labelLocation;
+        private System.Windows.Forms.Label labelFriendsCount;
+        private System.Windows.Forms.Label labelHometown;
+        private System.Windows.Forms.Label labelGender;
+        private System.Windows.Forms.Label labelWallPosts;
     }
 }
