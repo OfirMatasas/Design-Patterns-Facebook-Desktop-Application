834886c812057d7722ce21cbdf281c11e4009c60
Ido Levy <ido657@gmail.com>
2022-08-10 12:56:17 +0300

    statistics visible label

--- C22 Ex01 IdoLevy 208427963 OfirMatasas 205916729/FacebookWinFormsApp/Sub Forms/FormStatistics.Designer.cs
+++ C22 Ex01 IdoLevy 208427963 OfirMatasas 205916729/FacebookWinFormsApp/Sub Forms/FormStatistics.Designer.cs
@@ -35,22 +35,22 @@
             this.labelNumberOfPosts = new System.Windows.Forms.Label();
             this.labelMonthSummary = new System.Windows.Forms.Label();
             this.panelStatistics1 = new System.Windows.Forms.Panel();
+            this.pictureBoxLikesOnPhotos = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPosts = new System.Windows.Forms.PictureBox();
             this.labelNumberOfNewPhotos = new System.Windows.Forms.Label();
             this.labelNumberOfNewAlbums = new System.Windows.Forms.Label();
             this.panelStatistics2 = new System.Windows.Forms.Panel();
-            this.buttonShowStatistics = new System.Windows.Forms.Button();
-            this.panelSummary = new System.Windows.Forms.Panel();
             this.pictureBoxNewAlbums = new System.Windows.Forms.PictureBox();
             this.pictureBoxNewPhotos = new System.Windows.Forms.PictureBox();
-            this.pictureBoxLikesOnPhotos = new System.Windows.Forms.PictureBox();
-            this.pictureBoxPosts = new System.Windows.Forms.PictureBox();
+            this.buttonShowStatistics = new System.Windows.Forms.Button();
+            this.panelSummary = new System.Windows.Forms.Panel();
             this.panelStatistics1.SuspendLayout();
-            this.panelStatistics2.SuspendLayout();
-            this.panelSummary.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNewAlbums)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNewPhotos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLikesOnPhotos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPosts)).BeginInit();
+            this.panelStatistics2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNewAlbums)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNewPhotos)).BeginInit();
+            this.panelSummary.SuspendLayout();
             this.SuspendLayout();
             // 
             // dateTimePickerChosenDate
@@ -58,19 +58,21 @@
             this.dateTimePickerChosenDate.AllowDrop = true;
             this.dateTimePickerChosenDate.CustomFormat = "MM/yyyy";
             this.dateTimePickerChosenDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePickerChosenDate.Location = new System.Drawing.Point(408, 91);
+            this.dateTimePickerChosenDate.Location = new System.Drawing.Point(272, 59);
+            this.dateTimePickerChosenDate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimePickerChosenDate.Name = "dateTimePickerChosenDate";
             this.dateTimePickerChosenDate.ShowUpDown = true;
-            this.dateTimePickerChosenDate.Size = new System.Drawing.Size(108, 26);
+            this.dateTimePickerChosenDate.Size = new System.Drawing.Size(73, 20);
             this.dateTimePickerChosenDate.TabIndex = 1;
             // 
             // labelChooseDate
             // 
             this.labelChooseDate.AutoSize = true;
             this.labelChooseDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.labelChooseDate.Location = new System.Drawing.Point(132, 88);
+            this.labelChooseDate.Location = new System.Drawing.Point(88, 57);
+            this.labelChooseDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelChooseDate.Name = "labelChooseDate";
-            this.labelChooseDate.Size = new System.Drawing.Size(265, 29);
+            this.labelChooseDate.Size = new System.Drawing.Size(182, 20);
             this.labelChooseDate.TabIndex = 2;
             this.labelChooseDate.Text = "Please choose a date";
             this.labelChooseDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -78,29 +80,31 @@
             // labelNumberOfLikesOnPhotos
             // 
             this.labelNumberOfLikesOnPhotos.AutoSize = true;
-            this.labelNumberOfLikesOnPhotos.Location = new System.Drawing.Point(6, 100);
+            this.labelNumberOfLikesOnPhotos.Location = new System.Drawing.Point(4, 65);
+            this.labelNumberOfLikesOnPhotos.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelNumberOfLikesOnPhotos.Name = "labelNumberOfLikesOnPhotos";
-            this.labelNumberOfLikesOnPhotos.Size = new System.Drawing.Size(0, 21);
+            this.labelNumberOfLikesOnPhotos.Size = new System.Drawing.Size(0, 13);
             this.labelNumberOfLikesOnPhotos.TabIndex = 8;
             // 
             // labelNumberOfPosts
             // 
             this.labelNumberOfPosts.AutoSize = true;
-            this.labelNumberOfPosts.Location = new System.Drawing.Point(286, 100);
+            this.labelNumberOfPosts.Location = new System.Drawing.Point(191, 65);
+            this.labelNumberOfPosts.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelNumberOfPosts.Name = "labelNumberOfPosts";
-            this.labelNumberOfPosts.Size = new System.Drawing.Size(0, 21);
+            this.labelNumberOfPosts.Size = new System.Drawing.Size(0, 13);
             this.labelNumberOfPosts.TabIndex = 10;
             // 
             // labelMonthSummary
             // 
             this.labelMonthSummary.AutoSize = true;
             this.labelMonthSummary.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.labelMonthSummary.Location = new System.Drawing.Point(234, 9);
+            this.labelMonthSummary.Location = new System.Drawing.Point(156, 6);
+            this.labelMonthSummary.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelMonthSummary.Name = "labelMonthSummary";
-            this.labelMonthSummary.Size = new System.Drawing.Size(356, 46);
+            this.labelMonthSummary.Size = new System.Drawing.Size(246, 31);
             this.labelMonthSummary.TabIndex = 11;
             this.labelMonthSummary.Text = "Monthly Summary";
-            this.labelMonthSummary.Visible = false;
             // 
             // panelStatistics1
             // 
@@ -110,87 +114,17 @@
             this.panelStatistics1.Controls.Add(this.pictureBoxPosts);
             this.panelStatistics1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelStatistics1.Location = new System.Drawing.Point(0, 0);
+            this.panelStatistics1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panelStatistics1.Name = "panelStatistics1";
-            this.panelStatistics1.Size = new System.Drawing.Size(378, 188);
+            this.panelStatistics1.Size = new System.Drawing.Size(252, 122);
             this.panelStatistics1.TabIndex = 12;
-            // 
-            // labelNumberOfNewPhotos
-            // 
-            this.labelNumberOfNewPhotos.AutoSize = true;
-            this.labelNumberOfNewPhotos.Location = new System.Drawing.Point(286, 100);
-            this.labelNumberOfNewPhotos.Name = "labelNumberOfNewPhotos";
-            this.labelNumberOfNewPhotos.Size = new System.Drawing.Size(0, 21);
-            this.labelNumberOfNewPhotos.TabIndex = 13;
-            // 
-            // labelNumberOfNewAlbums
-            // 
-            this.labelNumberOfNewAlbums.AutoSize = true;
-            this.labelNumberOfNewAlbums.Location = new System.Drawing.Point(6, 100);
-            this.labelNumberOfNewAlbums.Name = "labelNumberOfNewAlbums";
-            this.labelNumberOfNewAlbums.Size = new System.Drawing.Size(0, 21);
-            this.labelNumberOfNewAlbums.TabIndex = 12;
-            // 
-            // panelStatistics2
-            // 
-            this.panelStatistics2.Controls.Add(this.labelNumberOfNewPhotos);
-            this.panelStatistics2.Controls.Add(this.pictureBoxNewAlbums);
-            this.panelStatistics2.Controls.Add(this.labelNumberOfNewAlbums);
-            this.panelStatistics2.Controls.Add(this.pictureBoxNewPhotos);
-            this.panelStatistics2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelStatistics2.Location = new System.Drawing.Point(0, 195);
-            this.panelStatistics2.Name = "panelStatistics2";
-            this.panelStatistics2.Size = new System.Drawing.Size(378, 188);
-            this.panelStatistics2.TabIndex = 14;
-            // 
-            // buttonShowStatistics
-            // 
-            this.buttonShowStatistics.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.buttonShowStatistics.Location = new System.Drawing.Point(530, 83);
-            this.buttonShowStatistics.Name = "buttonShowStatistics";
-            this.buttonShowStatistics.Size = new System.Drawing.Size(248, 44);
-            this.buttonShowStatistics.TabIndex = 15;
-            this.buttonShowStatistics.Text = "Show Statistics";
-            this.buttonShowStatistics.UseVisualStyleBackColor = true;
-            this.buttonShowStatistics.Click += new System.EventHandler(this.buttonShowStatistics_Click);
-            // 
-            // panelSummary
-            // 
-            this.panelSummary.Controls.Add(this.panelStatistics2);
-            this.panelSummary.Controls.Add(this.panelStatistics1);
-            this.panelSummary.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.panelSummary.Location = new System.Drawing.Point(228, 171);
-            this.panelSummary.Name = "panelSummary";
-            this.panelSummary.Size = new System.Drawing.Size(378, 383);
-            this.panelSummary.TabIndex = 16;
-            this.panelSummary.Visible = false;
-            // 
-            // pictureBoxNewAlbums
-            // 
-            this.pictureBoxNewAlbums.ErrorImage = null;
-            this.pictureBoxNewAlbums.Image = global::BasicFacebookFeatures.Properties.Resources.Statistics_Albums;
-            this.pictureBoxNewAlbums.Location = new System.Drawing.Point(0, 0);
-            this.pictureBoxNewAlbums.Name = "pictureBoxNewAlbums";
-            this.pictureBoxNewAlbums.Size = new System.Drawing.Size(64, 64);
-            this.pictureBoxNewAlbums.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBoxNewAlbums.TabIndex = 10;
-            this.pictureBoxNewAlbums.TabStop = false;
-            // 
-            // pictureBoxNewPhotos
-            // 
-            this.pictureBoxNewPhotos.ErrorImage = null;
-            this.pictureBoxNewPhotos.Image = global::BasicFacebookFeatures.Properties.Resources.Statistics_Photos;
-            this.pictureBoxNewPhotos.Location = new System.Drawing.Point(282, 0);
-            this.pictureBoxNewPhotos.Name = "pictureBoxNewPhotos";
-            this.pictureBoxNewPhotos.Size = new System.Drawing.Size(64, 64);
-            this.pictureBoxNewPhotos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBoxNewPhotos.TabIndex = 11;
-            this.pictureBoxNewPhotos.TabStop = false;
             // 
             // pictureBoxLikesOnPhotos
             // 
             this.pictureBoxLikesOnPhotos.ErrorImage = null;
             this.pictureBoxLikesOnPhotos.Image = global::BasicFacebookFeatures.Properties.Resources.Statistics_Likes;
             this.pictureBoxLikesOnPhotos.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxLikesOnPhotos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBoxLikesOnPhotos.Name = "pictureBoxLikesOnPhotos";
             this.pictureBoxLikesOnPhotos.Size = new System.Drawing.Size(64, 64);
             this.pictureBoxLikesOnPhotos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -201,35 +135,116 @@
             // 
             this.pictureBoxPosts.ErrorImage = null;
             this.pictureBoxPosts.Image = global::BasicFacebookFeatures.Properties.Resources.Statistics_Posts;
-            this.pictureBoxPosts.Location = new System.Drawing.Point(282, 0);
+            this.pictureBoxPosts.Location = new System.Drawing.Point(188, 0);
+            this.pictureBoxPosts.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBoxPosts.Name = "pictureBoxPosts";
             this.pictureBoxPosts.Size = new System.Drawing.Size(64, 64);
             this.pictureBoxPosts.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBoxPosts.TabIndex = 9;
             this.pictureBoxPosts.TabStop = false;
             // 
+            // labelNumberOfNewPhotos
+            // 
+            this.labelNumberOfNewPhotos.AutoSize = true;
+            this.labelNumberOfNewPhotos.Location = new System.Drawing.Point(191, 65);
+            this.labelNumberOfNewPhotos.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelNumberOfNewPhotos.Name = "labelNumberOfNewPhotos";
+            this.labelNumberOfNewPhotos.Size = new System.Drawing.Size(0, 13);
+            this.labelNumberOfNewPhotos.TabIndex = 13;
+            // 
+            // labelNumberOfNewAlbums
+            // 
+            this.labelNumberOfNewAlbums.AutoSize = true;
+            this.labelNumberOfNewAlbums.Location = new System.Drawing.Point(4, 65);
+            this.labelNumberOfNewAlbums.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelNumberOfNewAlbums.Name = "labelNumberOfNewAlbums";
+            this.labelNumberOfNewAlbums.Size = new System.Drawing.Size(0, 13);
+            this.labelNumberOfNewAlbums.TabIndex = 12;
+            // 
+            // panelStatistics2
+            // 
+            this.panelStatistics2.Controls.Add(this.labelNumberOfNewPhotos);
+            this.panelStatistics2.Controls.Add(this.pictureBoxNewAlbums);
+            this.panelStatistics2.Controls.Add(this.labelNumberOfNewAlbums);
+            this.panelStatistics2.Controls.Add(this.pictureBoxNewPhotos);
+            this.panelStatistics2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelStatistics2.Location = new System.Drawing.Point(0, 127);
+            this.panelStatistics2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelStatistics2.Name = "panelStatistics2";
+            this.panelStatistics2.Size = new System.Drawing.Size(252, 122);
+            this.panelStatistics2.TabIndex = 14;
+            // 
+            // pictureBoxNewAlbums
+            // 
+            this.pictureBoxNewAlbums.ErrorImage = null;
+            this.pictureBoxNewAlbums.Image = global::BasicFacebookFeatures.Properties.Resources.Statistics_Albums;
+            this.pictureBoxNewAlbums.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxNewAlbums.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBoxNewAlbums.Name = "pictureBoxNewAlbums";
+            this.pictureBoxNewAlbums.Size = new System.Drawing.Size(64, 64);
+            this.pictureBoxNewAlbums.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxNewAlbums.TabIndex = 10;
+            this.pictureBoxNewAlbums.TabStop = false;
+            // 
+            // pictureBoxNewPhotos
+            // 
+            this.pictureBoxNewPhotos.ErrorImage = null;
+            this.pictureBoxNewPhotos.Image = global::BasicFacebookFeatures.Properties.Resources.Statistics_Photos;
+            this.pictureBoxNewPhotos.Location = new System.Drawing.Point(188, 0);
+            this.pictureBoxNewPhotos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBoxNewPhotos.Name = "pictureBoxNewPhotos";
+            this.pictureBoxNewPhotos.Size = new System.Drawing.Size(64, 64);
+            this.pictureBoxNewPhotos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxNewPhotos.TabIndex = 11;
+            this.pictureBoxNewPhotos.TabStop = false;
+            // 
+            // buttonShowStatistics
+            // 
+            this.buttonShowStatistics.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.buttonShowStatistics.Location = new System.Drawing.Point(353, 54);
+            this.buttonShowStatistics.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonShowStatistics.Name = "buttonShowStatistics";
+            this.buttonShowStatistics.Size = new System.Drawing.Size(165, 29);
+            this.buttonShowStatistics.TabIndex = 15;
+            this.buttonShowStatistics.Text = "Show Statistics";
+            this.buttonShowStatistics.UseVisualStyleBackColor = true;
+            this.buttonShowStatistics.Click += new System.EventHandler(this.buttonShowStatistics_Click);
+            // 
+            // panelSummary
+            // 
+            this.panelSummary.Controls.Add(this.panelStatistics2);
+            this.panelSummary.Controls.Add(this.panelStatistics1);
+            this.panelSummary.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.panelSummary.Location = new System.Drawing.Point(152, 111);
+            this.panelSummary.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelSummary.Name = "panelSummary";
+            this.panelSummary.Size = new System.Drawing.Size(252, 249);
+            this.panelSummary.TabIndex = 16;
+            this.panelSummary.Visible = false;
+            // 
             // FormStatistics
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(981, 843);
+            this.ClientSize = new System.Drawing.Size(654, 548);
             this.Controls.Add(this.panelSummary);
             this.Controls.Add(this.buttonShowStatistics);
             this.Controls.Add(this.labelMonthSummary);
             this.Controls.Add(this.labelChooseDate);
             this.Controls.Add(this.dateTimePickerChosenDate);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormStatistics";
             this.Text = "Statistics";
             this.panelStatistics1.ResumeLayout(false);
             this.panelStatistics1.PerformLayout();
-            this.panelStatistics2.ResumeLayout(false);
-            this.panelStatistics2.PerformLayout();
-            this.panelSummary.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNewAlbums)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNewPhotos)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLikesOnPhotos)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPosts)).EndInit();
+            this.panelStatistics2.ResumeLayout(false);
+            this.panelStatistics2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNewAlbums)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNewPhotos)).EndInit();
+            this.panelSummary.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
